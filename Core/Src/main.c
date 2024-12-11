0143ce3c9d5153713bfacd4eb903adb177249499
KUXK <99731335+KUXK1@users.noreply.github.com>
2024-12-11 11:33:25 +0100

    Poprawka błędu

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,7 +27,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "functions.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
